8b400c93477d59a758e5d5469d678bbe3384cea9
roketag <roketag33@gmail.com>
2024-03-05 11:01:07 +0100

    push 03

---
+++ java/Java 03 Variables/Var.java
@@ -0,0 +1,9 @@
+public class Var {
+ public static void main(String[] args) {
+    String Author = "Steven Spielberg";
+    Boolean IsView = false;
+    int Year = 1900;
+    Double Rate = 3.14;
+    System.out.println(Author+" "+IsView+" "+Year+" "+Rate);
+ }
+}
